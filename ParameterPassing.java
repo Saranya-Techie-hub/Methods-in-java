69d9ec08553b55ed0040ee3ad041e1064f8993fe
Saranya-Techie-hub <saranyak.cs19@gmail.com>
2025-08-07 07:11:47 +0530

    object passed as a parameter

---
+++ ParameterPassing.java
@@ -0,0 +1,23 @@
+package MethodsInJava;
+import java.util.Scanner;
+
+public class ParameterPassing {
+    void update(int []arr, int index, int val)
+    {
+        arr[index] = val;
+    }
+
+    public static void main(String[] args) {
+        int []arr = {1, 3, 5, 7, 9, 11, 13};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter index to update");
+        int index = sc.nextInt();
+        System.out.println("Enter the value");
+        int val = sc.nextInt();
+        ParameterPassing mp = new ParameterPassing();
+        mp.update(arr, index, val);
+        System.out.println(arr[index]);
+        sc.close();
+    }
+    
+}
